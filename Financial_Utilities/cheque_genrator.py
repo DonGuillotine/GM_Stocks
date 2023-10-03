be715d34cc2c079ce5b4b05532073072bd8018a2
Donald Nwokoro <infect3dlab@gmail.com>
2023-10-03 22:49:41 +0100

    Updated convert_number_to_words() to return words
    I covered numbers from 1 to 999,999,999

--- Financial_Utilities/cheque_genrator.py
+++ Financial_Utilities/cheque_genrator.py
@@ -1,8 +1,57 @@
 from tkinter import *
 
 def convert_number_to_words(n):
-    words = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
-    return ' '.join(words[int(i)] for i in str(n))
+    if n == 0:
+        return "zero"
+    
+    units = ["", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+    teens = ["", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"]
+    tens = ["", "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"]
+    thousands = ["", "thousand", "million", "billion"]
+    
+    def rec_convert(num, idx):
+        if num == 0:
+            return ""
+        
+        words = []
+        if num > 99:
+            words.append(units[num // 100])
+            words.append("hundred")
+            num %= 100
+        
+        if num > 10 and num < 20:
+            words.append(teens[num-10])
+        else:
+            if num >= 20 or num == 10:
+                words.append(tens[num // 10])
+                num %= 10
+            
+            if num > 0:
+                words.append(units[num])
+        
+        if idx > 0 and any(words):
+            words.append(thousands[idx])
+        
+        return ' '.join(words)
+    
+    words = []
+    idx = 0
+    while n > 0:
+        segment = n % 1000
+        words.append(rec_convert(segment, idx))
+        n //= 1000
+        idx += 1
+    
+    words = [w for w in reversed(words) if w]
+    
+
+    if len(words) > 1:
+        return ', '.join(words[:-1]) + ' and ' + words[-1]
+    else:
+        return words[0]
+
+
+
 
 def cheque_generator():
     name = name_entry.get()
